10f229e9d87a9ffab0b46eb610aa7238c504ae38
VidalLuyo <jesus.luyoc@vallegrande.edu.pe>
2024-03-11 12:47:58 -0500

    Condicionales - Bucles

---
+++ 00_VidalLuyo/src/main/java/S03_00/S03_00_Array2.java
@@ -0,0 +1,16 @@
+package S03_00;
+
+/**
+ *
+ * @author Usuario
+ */
+public class S03_00_Array2 {
+    
+    public static void main(String[] args) {
+        
+        int[][] numeros2 = {{10, 20},{30, 40}};
+        
+        System.out.println(numeros2[1][0]);
+    }
+    
+}
