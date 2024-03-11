b244e0a92860d42c891359a10873449b38dc97f1
VidalLuyo <jesus.luyoc@vallegrande.edu.pe>
2024-03-11 13:05:14 -0500

    Proyecto While

---
+++ 00_VidalLuyo/src/main/java/S03_00/S03_00_While.java
@@ -0,0 +1,16 @@
+package S03_00;
+
+/**
+ *
+ * @author Usuario
+ */
+public class S03_00_While {
+
+    public static void main(String[] args) {
+        int a = 1;
+        while (a <= 10) {
+            System.out.println("hola");
+            a++;
+        }
+    }
+}
